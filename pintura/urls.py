ae4621fe0a6f78ad8d98a64b5a14af8107ac89d1
PedroMarquesjc <pedromarques.jc@gmail.com>
2024-01-29 12:03:33 -0300

    Adicionando arquivos estáticos, css e página de resposta.

--- pintura/urls.py
+++ pintura/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calculo_pintura/',views.pintura_inicio)
+    path('calculo_pintura/',views.pintura_inicio),
+    path('resultado_pintura/',views.pintura_inicio)
 ]
--- pintura/views.py
+++ pintura/views.py
@@ -3,12 +3,23 @@ from django.http import HttpResponse
 
 def pintura_inicio(request):
     if request.method == "GET":
-        print(request.method)
         return render(request,'html/index.html')
+    
     elif request.method =="POST":
         largura = request.POST.get('Largura')
         comprimento = request.POST.get('Comprimento')
         rendimento = request.POST.get('Rendimento')
         resultado = float(largura) * float(comprimento)
         total = float(resultado) / float(rendimento)
-        return HttpResponse(f'Com a Largura {largura} metros, e o comprimento {comprimento} metros, a metragem quadrada é: {resultado:.2f} metros quadrados. Será necessário {total:.1f} litros de tinta.')
+       
+        
+        context = {
+            'Largura' : largura ,
+            'Comprimento' : comprimento,
+            'Resultado' : str(resultado),
+            'Rendimento' : rendimento,
+            'Total' : str(total),
+        }
+     
+        return render(request,'html/resultado_pintura.html', context)
+
